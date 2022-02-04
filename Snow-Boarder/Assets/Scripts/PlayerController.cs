2ce8c9de95d4c5b0b86b14141dcec597222fa966
KilluasYoYo <98287025+KilluasYoYo@users.noreply.github.com>
2022-02-04 22:56:44 +0300

    If you push uparrow key you can boost your speed

--- Snow-Boarder/Assets/Scripts/PlayerController.cs
+++ Snow-Boarder/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float torqueAmount = 17.5f;
+    [SerializeField] float baseSpeed = 16f;
+    [SerializeField] float boostSpeed = 25f;
     Rigidbody2D rb2d;
-    // Start is called before the first frame update
+    SurfaceEffector2D surfaceEffector;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector = FindObjectOfType<SurfaceEffector2D>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector.speed = baseSpeed;
+        }   
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
         }
 
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
         }
