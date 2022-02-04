e8e668a0403d44c85df08995001ccb5b552b5a77
KilluasYoYo <98287025+KilluasYoYo@users.noreply.github.com>
2022-02-04 04:58:07 +0300

    I added particle effects for finish line and head crashing. Also we have a new and better level now.

--- Snow-Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow-Boarder/Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField]ParticleSystem crashEffect;
+    [SerializeField]float delayAmount=1;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
-            SceneManager.LoadScene(0);
+            crashEffect.Play();
+            Invoke("ReloadSceneOnCrash",delayAmount);
+            Debug.Log("Ouch! My head!");
         }
+    }
+
+    void ReloadSceneOnCrash()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Snow-Boarder/Assets/Scripts/FinishLine.cs
+++ Snow-Boarder/Assets/Scripts/FinishLine.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField]float delayAmount=1;
+    [SerializeField]ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene(0);
-            Debug.Log("You Finished!!!");
+            finishEffect.Play();
+            Invoke("ReloadSceneOnFinishLine",delayAmount);
         }
+    }
+    void ReloadSceneOnFinishLine()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("I finished. Finally...");
     }
 }
