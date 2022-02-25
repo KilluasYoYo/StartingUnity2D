9b21093e8bd245f688004a22e44f2fcbfc8d3403
KilluasYoYo <98287025+KilluasYoYo@users.noreply.github.com>
2022-02-26 01:50:56 +0300

    Added coin pick up system and coin pick up sfx

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickUpSFX;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
             Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
         }    
     }
 }
